12ac6bc3837cb97189736cb3f650a43bfbc27077
Adie <Kiyiko@gmail.com>
2021-02-05 00:59:31 -0600

    Change from urllib to requests and add headers to fix 403 error

--- src/downloaders/redgifs.py
+++ src/downloaders/redgifs.py
@@ -1,6 +1,6 @@
 import json
 import os
-import urllib.request
+import requests
 from bs4 import BeautifulSoup
 
 from src.downloaders.downloaderUtils import getFile, getExtension
@@ -29,7 +29,12 @@ class Redgifs:
         """Extract direct link to the video from page's source
         and return it
         """
-
+        hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
         if '.webm' in url or '.mp4' in url or '.gif' in url:
             return url
 
@@ -37,8 +42,9 @@ class Redgifs:
             url = url[:-1]
 
         url = "https://redgifs.com/watch/" + url.split('/')[-1]
-
-        pageSource = (urllib.request.urlopen(url).read().decode())
+        print("URL:", url)
+        response = requests.get(url, headers=hdr)
+        pageSource = response.text
 
         soup = BeautifulSoup(pageSource, "html.parser")
         attributes = {"data-react-helmet":"true","type":"application/ld+json"}
@@ -46,5 +52,5 @@ class Redgifs:
 
         if content is None:
             raise NotADownloadableLinkError("Could not read the page source")
-
+        print(json.loads(content.contents[0])["video"]["contentUrl"])
         return json.loads(content.contents[0])["video"]["contentUrl"]
