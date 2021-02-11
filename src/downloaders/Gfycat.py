e30e379b3eda63ce61466387418c0af9533f01da
Adie <Kiyiko@gmail.com>
2021-02-11 16:40:18 -0600

    Break down the page source retrieval process a little bit

--- src/downloaders/Gfycat.py
+++ src/downloaders/Gfycat.py
@@ -4,9 +4,9 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 from src.downloaders.downloaderUtils import getFile, getExtension
-from src.errors import (FileNameTooLong, AlbumNotDownloadedCompletely, 
+from src.errors import (FileNameTooLong, AlbumNotDownloadedCompletely,
                         NotADownloadableLinkError, FileAlreadyExistsError)
-from src.utils import GLOBAL
+from src.utils import GLOBAL, httpResponseCodeCheck
 from src.utils import printToFile as print
 from src.downloaders.gifDeliveryNetwork import GifDeliveryNetwork
 
@@ -18,13 +18,13 @@ class Gfycat:
             raise NotADownloadableLinkError("Could not read the page source")
 
         POST['EXTENSION'] = getExtension(POST['MEDIAURL'])
-        
+
         if not os.path.exists(directory): os.makedirs(directory)
 
-        filename = GLOBAL.config['filename'].format(**POST)+POST["EXTENSION"]
-        shortFilename = POST['POSTID']+POST['EXTENSION']
-       
-        getFile(filename,shortFilename,directory,POST['MEDIAURL'])
+        filename = GLOBAL.config['filename'].format(**POST) + POST["EXTENSION"]
+        shortFilename = POST['POSTID'] + POST['EXTENSION']
+
+        getFile(filename, shortFilename, directory, POST['MEDIAURL'])
 
     @staticmethod
     def getLink(url):
@@ -37,14 +37,18 @@ class Gfycat:
 
         if url[-1:] == '/':
             url = url[:-1]
-
         url = "https://gfycat.com/" + url.split('/')[-1]
+        req = urllib.request.Request(url)
+        req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36 OPR/54.0.2952.64')
 
-        pageSource = (urllib.request.urlopen(url).read().decode())
+        response = urllib.request.urlopen(req)
+        httpResponseCodeCheck(response.getcode(), url)
+        pageSource = response.read().decode()
 
         soup = BeautifulSoup(pageSource, "html.parser")
-        attributes = {"data-react-helmet":"true","type":"application/ld+json"}
-        content = soup.find("script",attrs=attributes)
+        attributes = {"data-react-helmet": "true",
+                      "type": "application/ld+json"}
+        content = soup.find("script", attrs=attributes)
 
         if content is None:
             return GifDeliveryNetwork.getLink(url)
