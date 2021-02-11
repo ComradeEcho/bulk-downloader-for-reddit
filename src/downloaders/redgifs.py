dd7e192bda7e60b356e7c62329937489d9fb5e06
Adie <Kiyiko@gmail.com>
2021-02-11 16:35:29 -0600

    Revert back to urllib to remove requests dependency

--- src/downloaders/redgifs.py
+++ src/downloaders/redgifs.py
@@ -1,16 +1,17 @@
 import json
 import os
-import requests
+import urllib.request
 from bs4 import BeautifulSoup
 
 from src.downloaders.downloaderUtils import getFile, getExtension
-from src.errors import (FileNameTooLong, AlbumNotDownloadedCompletely, 
-                        NotADownloadableLinkError, FileAlreadyExistsError)
-from src.utils import GLOBAL
+from src.errors import (FileNameTooLong, AlbumNotDownloadedCompletely,
+                        NotADownloadableLinkError, FileNotFoundError)
+from src.utils import GLOBAL, httpResponseCodeCheck
 from src.utils import printToFile as print
 
+
 class Redgifs:
-    def __init__(self,directory,POST):
+    def __init__(self, directory, POST):
         try:
             POST['MEDIAURL'] = self.getLink(POST['CONTENTURL'])
         except IndexError:
@@ -22,19 +23,14 @@ class Redgifs:
 
         filename = GLOBAL.config['filename'].format(**POST)+POST["EXTENSION"]
         shortFilename = POST['POSTID']+POST['EXTENSION']
-        
-        getFile(filename,shortFilename,directory,POST['MEDIAURL'])
+
+        getFile(filename, shortFilename, directory, POST['MEDIAURL'])
 
     def getLink(self, url):
         """Extract direct link to the video from page's source
         and return it
         """
-        hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-       'Accept-Encoding': 'none',
-       'Accept-Language': 'en-US,en;q=0.8',
-       'Connection': 'keep-alive'}
+        
         if '.webm' in url or '.mp4' in url or '.gif' in url:
             return url
 
@@ -42,9 +38,13 @@ class Redgifs:
             url = url[:-1]
 
         url = "https://redgifs.com/watch/" + url.split('/')[-1]
-        print("URL:", url)
-        response = requests.get(url, headers=hdr)
-        pageSource = response.text
+
+        req = urllib.request.Request(url)
+        req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36 OPR/54.0.2952.64')
+
+        response = urllib.request.urlopen(req)
+        httpResponseCodeCheck(response.getcode(), url)
+        pageSource = response.read().decode()
 
         soup = BeautifulSoup(pageSource, "html.parser")
         attributes = {"data-react-helmet":"true","type":"application/ld+json"}
@@ -52,5 +52,4 @@ class Redgifs:
 
         if content is None:
             raise NotADownloadableLinkError("Could not read the page source")
-        print(json.loads(content.contents[0])["video"]["contentUrl"])
         return json.loads(content.contents[0])["video"]["contentUrl"]
